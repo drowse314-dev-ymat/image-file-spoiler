5d1db60ca94037701737ed709ffacfebf025d2fa
yu.matsushita <yu.matsushita@flect.co.jp>
2018-06-05 02:31:37 +0900

    実装: `PublishImageFileSpoilerMessage` ユースケース実装: テストを通すまで(GREEN)

--- src/domain/usecases/publish-image-file-spoiler-message.ts
+++ src/domain/usecases/publish-image-file-spoiler-message.ts
@@ -1,11 +1,15 @@
 "use strict";
 
 import { inject, injectable } from "inversify";
+import * as _ from "lodash";
 
 import { ROLES } from "../roles";
-import { NewImageFileEventProvider } from "../image-file";
-import { ImageRecognizer } from "../image-recognizer";
-import { ImageFileSpoilerMessageSink } from "../image-file-spoiler-message";
+import { NewImageFileEventProvider, NewImageFileEvent } from "../image-file";
+import { ImageRecognizer, ObjectLabelsInImage } from "../image-recognizer";
+import {
+    ImageFileSpoilerMessageSink,
+    ImageFileSpoilerMessage
+} from "../image-file-spoiler-message";
 
 @injectable()
 export class PublishImageFileSpoilerMessage {
@@ -19,11 +23,87 @@ export class PublishImageFileSpoilerMessage {
     ) {}
 
     public invoke(): Promise<void> {
-        // fake reference
-        this.newImageFileEventProvider;
-        this.imageRecognizer;
-        this.imageFileSpoilerMessageSink;
+        return this.newImageFileEventProvider
+            .get()
+            .then(newImageFileEvent =>
+                newImageFileEvent
+                    .getImageFileRef()
+                    .then(imageFileRef =>
+                        this.imageRecognizer.findObjects(imageFileRef)
+                    )
+                    .then(objectLabelsInImage =>
+                        this.composeImageFileSpoilerMessage(
+                            newImageFileEvent,
+                            objectLabelsInImage
+                        )
+                    )
+            )
+            .then(message => this.imageFileSpoilerMessageSink.publish(message));
+    }
 
-        return Promise.resolve();
+    private composeImageFileSpoilerMessage(
+        _newImageFileEvent: NewImageFileEvent,
+        objectLabelsInImage: ObjectLabelsInImage
+    ): Promise<ImageFileSpoilerMessage> {
+        const maxConfidence = Math.max(
+            ...objectLabelsInImage.labelsWithConfidence.map(kv => kv.confidence)
+        );
+
+        const toStatementExpr = (predicates: string[]) =>
+            predicates.length === 0
+                ? "no_idea"
+                : predicates.length > 1
+                    ? `or(${predicates.join(", ")})`
+                    : predicates[0];
+
+        if (maxConfidence >= 0.9) {
+            const pickups = _.sortBy(
+                objectLabelsInImage.labelsWithConfidence.filter(
+                    kv => kv.confidence >= 0.9
+                ),
+                kv => -1 * kv.confidence
+            );
+            return Promise.resolve({
+                formalExpr: toStatementExpr(
+                    pickups.map(kv => `certainly(${kv.label})`)
+                ),
+                message: `${pickups.map(kv => kv.label).join("か")}です！`
+            });
+        }
+        if (maxConfidence >= 0.7) {
+            const pickups = _.sortBy(
+                objectLabelsInImage.labelsWithConfidence.filter(
+                    kv => kv.confidence >= 0.7
+                ),
+                kv => -1 * kv.confidence
+            );
+            return Promise.resolve({
+                formalExpr: toStatementExpr(
+                    pickups.map(kv => `probably(${kv.label})`)
+                ),
+                message: `${pickups
+                    .map(kv => kv.label)
+                    .join("か")}だと思われます。`
+            });
+        }
+        if (maxConfidence > 0.5) {
+            const pickups = _.sortBy(
+                objectLabelsInImage.labelsWithConfidence.filter(
+                    kv => kv.confidence > 0.5
+                ),
+                kv => -1 * kv.confidence
+            );
+            return Promise.resolve({
+                formalExpr: toStatementExpr(
+                    pickups.map(kv => `possibly(${kv.label})`)
+                ),
+                message: `${pickups.map(kv => kv.label).join("か")}...？`
+            });
+        }
+
+        return Promise.resolve({
+            formalExpr: toStatementExpr([]),
+            message: "わかりません.."
+        });
     }
 }
